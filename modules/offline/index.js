e1166254a34d0c104b9c3b1baf0eb8295bd9ecd0
Pooya Parsa <pooya@pi0.ir>
2017-05-30 15:29:30 +0430

    fix(offline): Use proper asset regex based on build.filenames

--- modules/offline/index.js
+++ modules/offline/index.js
@@ -3,48 +3,55 @@ const path = require('path')
 
 // https://github.com/NekR/offline-plugin/blob/master/docs/examples/SPA.md
 // https://github.com/NekR/offline-plugin-pwa
-const defaults = {
-  safeToUseOptionalCaches: true,
-
-  externals: [
-    '/'
-  ],
-
-  AppCache: {
-    events: true,
-    FALLBACK: {'/': '/offline'}
-  },
-
-  caches: {
-    main: [
-      'app.*.js',
-      'vendor.*.js',
-      '/'
-    ],
-    additional: [
-      '*.js'
-    ],
-    optional: [
-      ':rest:'
-    ]
-  },
-
-  ServiceWorker: {
-    events: true,
-    cacheName: process.env.npm_package_name, // Allows having multiply services in same domain
-    navigateFallbackURL: '/'
-  }
-}
 
 module.exports = function nuxtOffline(options) {
-  const useOffline = this.options.offline !== false && !this.options.dev
-  if (useOffline) {
-    // Default offline options
-    this.options.offline = _.defaultsDeep({}, this.options.offline, defaults)
-
-    // Register plugin
-    this.addPlugin({src: path.resolve(__dirname, 'plugin.js'), ssr: false})
+  if (this.options.offline === false || this.options.dev) {
+    return
   }
+
+  // common.[chunkhash].css ~> common.*.css
+  const baseName = pattern => pattern.replace(/\[\w+]/g,'*')
+
+  // Default offline options
+  const defaults = {
+    safeToUseOptionalCaches: true,
+
+    externals: [
+      '/'
+    ],
+
+    AppCache: {
+      events: true,
+      FALLBACK: {'/': '/offline'}
+    },
+
+    caches: {
+      main: [
+        baseName(this.options.build.filenames.css),
+        baseName(this.options.build.filenames.manifest),
+        baseName(this.options.build.filenames.vendor),
+        baseName(this.options.build.filenames.app),
+        '/'
+      ],
+      additional: [
+        '*.js'
+      ],
+      optional: [
+        ':rest:'
+      ]
+    },
+
+    ServiceWorker: {
+      events: true,
+      cacheName: process.env.npm_package_name, // Allows having multiply services in same domain
+      navigateFallbackURL: '/'
+    }
+  }
+  this.options.offline = _.defaultsDeep({}, this.options.offline, defaults)
+
+  // Register plugin
+  this.addPlugin({src: path.resolve(__dirname, 'plugin.js'), ssr: false})
+
 }
 
 module.exports.meta = require('./package.json')
