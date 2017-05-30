bb2e35a76a8b9eb53c0bc26ce04ae0c3bd798fd1
Ruslan Konev <konev.lincor@gmail.com>
2017-05-30 11:07:15 +0300

    Update plugin.js

--- modules/yandex-metrika/plugin.js
+++ modules/yandex-metrika/plugin.js
@@ -1,9 +1,17 @@
-
+try {
+  (w[c] = w[c] || []).push(function() {
+    window[`yaCounter${<%= options.id %>}`] = new Ya.Metrika(<%= options %>);
+  })(document, window, "yandex_metrika_callbacks");
+} catch (e) {}
 
 export default ({app: {router}}) => {
-  try {
-    (w[c] = w[c] || []).push(function() {
-      var yaCounter = new Ya.Metrika(<%= options %>);
-    })(document, window, "yandex_metrika_callbacks");
-  } catch (e) {}
+  if (!window['Ya'] || window[`yaCounter${<%= options.id %>}`]) {
+    console.warn('Yandex metrika is not available')
+    return
+  }
+    
+  router.afterEach((to, from) => {
+    // We tell Yandex Metrika to add a page view
+    window[`yaCounter${<%= options.id %>}`].hit(to.fullPath)
+  })
 }
