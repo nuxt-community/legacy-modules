a4e3a04a8fbea51dba45a1e90e38175a6444cfa9
Pooya Parsa <pooya@pi0.ir>
2017-06-02 22:29:37 +0430

    feat(meta): more meta tags
    Borrowed from Vue HN <3

--- modules/meta/index.js
+++ modules/meta/index.js
@@ -2,9 +2,14 @@ const path = require('path')
 
 module.exports = function nuxtMeta(options) {
   // Add some common meta tags
+  // Borrowed from Vue HN!
   // TODO: Make it more optional and flexible
+
   this.options.head.meta.push({charset: 'utf-8'})
-  this.options.head.meta.push({name: 'viewport', content: 'width=device-width, initial-scale=1'})
+  this.options.head.meta.push({name: 'viewport', content: 'width=device-width, initial-scale=1, minimal-ui'})
+  this.options.head.meta.push({name: 'mobile-web-app-capable', content: 'yes'})
+  this.options.head.meta.push({name: 'apple-mobile-web-app-capable', content: 'yes'})
+  this.options.head.meta.push({name: 'apple-mobile-web-app-status-bar-style', content: 'default'})
 }
 
 module.exports.meta = require('./package.json')
