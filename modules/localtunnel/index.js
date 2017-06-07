54a37a6ff6cf1ca8617397f0f2ffb77ee3e144fd
Pooya Parsa <pooya@pi0.ir>
2017-06-08 02:21:40 +0430

    fix(localtunnel): default host to 'localhost'

--- modules/localtunnel/index.js
+++ modules/localtunnel/index.js
@@ -2,14 +2,14 @@ const localtunnel = require('localtunnel')
 
 // https://github.com/localtunnel/localtunnel
 
-module.exports = function nuxtLocaltunnel(options) {
+module.exports = function nuxtLocaltunnel (options) {
   // Only include on dev mode
   if (!this.options.dev) {
     return
   }
 
   const port = process.env.PORT || process.env.npm_package_config_nuxt_port || 3000
-  const host = process.env.HOST || process.env.npm_package_config_nuxt_host
+  const host = process.env.HOST || process.env.npm_package_config_nuxt_host || 'localhost'
 
   const opts = {
     subdomain: options.subdomain || process.env.npm_package_name,
