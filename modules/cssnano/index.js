1b60e3b176ebdfeaa7ae6b0cabaac25e57e8a981
Pooya Parsa <pooya@pi0.ir>
2017-07-31 22:06:15 +0430

    fix(cssnano): handle when build.postcss is null
    #98

--- modules/cssnano/index.js
+++ modules/cssnano/index.js
@@ -1,6 +1,15 @@
 const cssnano = require('cssnano')
+const autoprefixer = require('autoprefixer')
 
-module.exports = function nuxtCssano(options) {
+module.exports = function nuxtCssano (options) {
+  if (!Array.isArray(this.options.build.postcss)) {
+    this.options.build.postcss = [
+      autoprefixer({
+        browsers: ['last 3 versions']
+      })
+    ]
+  }
+
   this.options.build.postcss.push(cssnano(options))
 }
 
