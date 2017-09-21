23b7b58371f9d313830fc05cfa9e3c16982e53aa
Ilya Semenov <ilya@semenov.co>
2017-09-22 00:02:24 +0700

    fix(yandex-metrika): fix undefined vars, prevent race condition (#143)
    #146

--- modules/yandex-metrika/plugin.js
+++ modules/yandex-metrika/plugin.js
@@ -6,14 +6,22 @@
   });
 })(document, window, "yandex_metrika_callbacks");
 
-export default ({app: {router}}) => {
-  if (!window['Ya'] || window['yaCounter<%= options.id %>']) {
-    console.warn('Yandex metrika is not available')
-    return
+
+  function create () {
+    try {
+      window['yaCounter<%= options.id %>'] = new Ya.Metrika(<%= JSON.stringify(options) %>)
+      app.router.afterEach(to => {
+        // We tell Yandex Metrika to add a page view
+        window['yaCounter<%= options.id %>'].hit(to.fullPath)
+      })
+    } catch (e) { }
   }
 
-  router.afterEach((to, from) => {
-    // We tell Yandex Metrika to add a page view
-    window['yaCounter<%= options.id %>'].hit(to.fullPath)
-  })
+  if (window.Ya && window.Ya.Metrika) {
+    create()
+  } else {
+    (function (w, c) {
+      (w[c] = w[c] || []).push(create)
+    })(window, 'yandex_metrika_callbacks')
+  }
 }
