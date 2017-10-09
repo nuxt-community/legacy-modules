fcea92149fe50c6d51860c4a31d9bb6490343b70
Troy Morehouse <troymore@nbnet.nb.ca>
2017-10-08 23:15:35 -0300

    Update index.js

--- packages/google-analytics/index.js
+++ packages/google-analytics/index.js
@@ -1,10 +1,25 @@
 const { resolve } = require('path')
 
-export default async function module (moduleOptions) {
-  const options = this.options['google-adsense'] || moduleOptions
+module.exports = function nuxtAdSense (moduleOptions) {
+  const opts = this.moduleOptions['google-adsense']
 
+  if (!opts || !opts[id]) {
+    console.warn('adsense client ID not specified')
+    return;
+  }
+
+  // Register our plugin
   this.addPlugin({
-    src: resolve(__dirname, '../templates/plugin.js'),
-    options
+    src: resolve(__dirname, '../plugin-template.js'),
+    { id: opts[id] }
+  })
+
+  // Add the google adsense script
+  this.moduleOptions.head.script.push({
+    hid: 'adsbygoogle',
+    async: true,
+    src: '//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js'
   })
 }
+
+module.exports.meta = require('./package.json')
