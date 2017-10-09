b1196b2e1960d12b4535a32ce73f31595b5c7e35
Troy Morehouse <troymore@nbnet.nb.ca>
2017-10-08 23:31:14 -0300

    Update plugin.js

--- packages/google-analytics/plugin.js
+++ packages/google-analytics/plugin.js
@@ -1,4 +1,16 @@
 import Vue from 'vue'
 import AdsByGoogle from './adsbygoogle.vue'
 
-Vue.component('adsbygoogle', AdsByGoogle)
+// Extend the AdsByGoogle component with the client ID
+const adsbygoogle = {
+  extends: AdsByGoogle,
+  props: {
+    adClient: {
+      type: String,
+      default: '<% options.id %>'
+    }
+  }
+}
+
+// Register our ad component
+Vue.component('adsbygoogle', adsbygoogle)
