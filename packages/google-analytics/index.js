25b6f91c9d520a023346a53f0f4594c861b335ec
Troy Morehouse <troymore@nbnet.nb.ca>
2017-10-09 00:15:43 -0300

    Update index.js

--- packages/google-analytics/index.js
+++ packages/google-analytics/index.js
@@ -1,30 +1,36 @@
 const { resolve } = require('path')
 
 module.exports = function nuxtAdSense (moduleOptions) {
-  const options = this.moduleOptions['google-adsense']
+  const options = Object.assign({}, this.moduleOptions['google-adsense'] || {})
 
-  if (!options || !options[id] || typeof options[id] !== 'string') {
-    console.warn('adsense client ID not specified')
-    return
-  }
-
-  // If in DEV mode, place ads in 'test' state
   if (this.options.dev && process.env.NODE_ENV !== 'production')) {
+    // If in DEV mode, place ads in 'test' state
     options.dev = true
+    options.id = 'ca-google'
+  } else if (!options[id] || typeof options[id] !== 'string') {
+    console.warn('Invalid adsense client ID specified')
+    return
   }
 
   // Register our plugin
   this.addPlugin({
-    src: resolve(__dirname, '../plugin.js'),
+    src: resolve(__dirname, './plugin.js'),
     options: options
   })
 
   // Add the google adsense script
-  this.moduleOptions.head.script.push({
-    hid: 'adsbygoogle',
+  this.options.head.script.push({
     async: true,
     src: '//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js'
   })
+  
+  if (options.test) {
+    // If in DEV mode, add robots meta first to comply with Adsense policies
+    this.options.head.meta.unshift({
+      name: 'robots',
+      content: 'noindex,noarchive,nofollow'
+    })
+  }
 }
 
 module.exports.meta = require('./package.json')
