0cf4ea56bfdcd666b04721b8dfd7e00093667549
Pooya Parsa <pooya@pi0.ir>
2017-10-26 21:22:29 +0330

    chore: yarn workspaces

--- index.js
+++
@@ -1,9 +0,0 @@
-function warn() {
-  console.warn('[nuxt] please use individual nuxt module packages')
-  console.warn('https://github.com/nuxt/modules')
-}
-
-module.exports = function wrap(a, b) {
-  warn()
-  return b || a || {};
-}
