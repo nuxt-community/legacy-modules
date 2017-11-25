c8ba500ecbe100990bea3b202e4a168ccdfa390a
Pooya Parsa <pooya@pi0.ir>
2017-11-25 17:11:46 +0330

    chore(vendor): support nuxt 1.0.0

--- packages/vendor/index.js
+++ packages/vendor/index.js
@@ -10,13 +10,22 @@ module.exports = function nuxtVendor (moduleOptions) {
 
   const vendorDir = path.resolve(this.options.srcDir, 'static', 'vendor')
 
+  const modulesDir = Array.isArray(this.options.modulesDir) ? this.options.modulesDir : [this.options.modulesDir]
+
   const vendor = [].concat(this.options.vendor, moduleOptions.vendor)
     .filter(v => v)
-    .map(v => ({
-      src: v.src || path.resolve(this.options.modulesDir, v),
-      dst: v.dst || path.resolve(vendorDir, v)
-    }))
-    .filter(v => fs.existsSync(v.src))
+    .map(v => {
+      for (let dir of modulesDir) {
+        const src = path.resolve(dir, v.src || v)
+        if (fs.existsSync(src)) {
+          return {
+            src,
+            dst: v.dst || path.resolve(vendorDir, v)
+          }
+        }
+      }
+    })
+    .filter(v => v)
 
   if (!vendor.length) {
     return
@@ -26,7 +35,7 @@ module.exports = function nuxtVendor (moduleOptions) {
   fs.ensureDirSync(vendorDir)
 
   // Link vendors
-  vendor.forEach(({src, dst}) => {
+  vendor.forEach(({ src, dst }) => {
     fs.removeSync(dst)
     fs.ensureSymlinkSync(src, dst, 'junction')
   })
