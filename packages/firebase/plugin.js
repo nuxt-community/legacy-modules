f94d34a14561908d7e0d7d9a0da21ec32869f9a9
kotamat <kota1861@gmail.com>
2017-11-27 09:24:38 +0900

    add projectId

--- packages/firebase/plugin.js
+++ packages/firebase/plugin.js
@@ -14,6 +14,7 @@ const firebasePlugin = {
         apiKey: process.env.apiKey || '<%= options.apiKey %>',
         authDomain: process.env.authDomain || '<%= options.authDomain %>',
         databaseURL: process.env.databaseURL || '<%= options.databaseURL %>',
+        projectId: process.env.projectId || '<%= options.projeectId %>',
         storageBucket: process.env.storageBucket || '<%= options.storageBucket %>'
       })
     }
