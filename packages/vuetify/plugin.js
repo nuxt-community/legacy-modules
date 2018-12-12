3e25477ffcbc7c7f2d842dbf050aec97ad2d2845
Mark <me@mrkstwrt.com>
2018-12-12 11:36:20 +0000

    feat(vuetify): add tree-shaking option (#242)

--- packages/vuetify/plugin.js
+++ packages/vuetify/plugin.js
@@ -1,4 +1,8 @@
 import Vue from 'vue'
-import Vuetify from 'vuetify'
+<% if (options.treeShake) { %>
+import Vuetify from 'vuetify/lib';
+<% } else { %>
+import Vuetify from 'vuetify';
+<% } %>
 
 Vue.use(Vuetify, <%= JSON.stringify(options.vuetifyOptions, null, 2) %>)
