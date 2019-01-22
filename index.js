bca2d70635dd87f9fffa0c9074538ecf31701ce8
Аркадий Кожедуб <arkadij.ok@gmail.com>
2019-01-22 18:15:53 +0700

    fix: html validate error (#253)

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ module.exports = function yandexMetrika (options) {
   // Add yandex metrika script to head
   this.options.head.script.push({
     src: (options.useCDN ? 'https://cdn.jsdelivr.net/npm/yandex-metrica-watch' : 'https://mc.yandex.ru/metrika') + '/watch.js', // add https://cdn.jsdelivr.net/npm/yandex-metrica-watch/watch.js
-    async: true
+    async: ''
   })
 
   // Register plugin
