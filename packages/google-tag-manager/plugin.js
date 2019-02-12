00bc555a468567104503588819504f85841ca7df
William Chong <williamchong007@msn.com>
2019-02-12 18:22:44 +0800

    fix(google-tag-manager): fix broken syntax

--- packages/google-tag-manager/plugin.js
+++ packages/google-tag-manager/plugin.js
@@ -1,27 +1,31 @@
+// Include Google Tag Manager Script
+function setup() {
+  window['<%= options.layer %>'] = window['<%= options.layer %>'] || [];
+  window['<%= options.layer %>'].push({
+    event: 'gtm.js', 'gtm.start': new Date().getTime()
+  });
+}
+
+const shouldCheckDNT = <%= options.respectDoNotTrack %>
 // Detect Do Not Track settings
-<% if (options.respectDoNotTrack) { %>
-if (
-  window.doNotTrack === '1'
+const hasDNT = shouldCheckDNT && (window.doNotTrack === '1'
   || navigator.doNotTrack === 'yes'
   || navigator.doNotTrack === '1'
   || navigator.msDoNotTrack === '1'
   || (window.external && window.external.msTrackingProtectionEnabled && window.external.msTrackingProtectionEnabled())
-) return;
-<% } %>
+)
 
-// Include Google Tag Manager Script
-window['<%= options.layer %>'] = window['<%= options.layer %>'] || [];
-window['<%= options.layer %>'].push({
-  event: 'gtm.js', 'gtm.start': new Date().getTime()
-});
+if (!hasDNT) setup()
 
 <% if (options.pageTracking) { %>
 // Every time the route changes (fired on initialization too)
 export default ({ app: { router } }) => {
-    router.afterEach((to, from) => {
-      setTimeout(() => {
-        window['<%= options.layer %>'].push(to.gtm || { event: 'nuxtRoute', pageType: 'PageView', pageUrl: to.fullPath, routeName: to.name })
-      }, 0)
-    })
+    if (!hasDNT) {
+      router.afterEach((to, from) => {
+        setTimeout(() => {
+          window['<%= options.layer %>'].push(to.gtm || { event: 'nuxtRoute', pageType: 'PageView', pageUrl: to.fullPath, routeName: to.name })
+        }, 0)
+      })
+    }
   }
 <% } %>
