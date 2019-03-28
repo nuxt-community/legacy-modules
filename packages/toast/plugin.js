10c9b3030f7f90674304a5da8dd049c16ac19dbf
mcoope31 <mscoops@gmail.com>
2019-03-27 20:52:48 -0500

    Adding ability to register custom toasts

--- packages/toast/plugin.js
+++ packages/toast/plugin.js
@@ -1,7 +1,18 @@
 import Vue from 'vue'
 import Toasted from 'vue-toasted'
 
-Vue.use(Toasted, <%= serialize(options) %>)
+const opts = <%= serialize(options) %>
+var globals = []
+if(opts.hasOwnProperty('register')) {
+  globals = opts.register
+  delete opts.register
+}
+
+Vue.use(Toasted, opts)
+
+globals.forEach(global => {
+  Vue.toasted.register(global.name, global.message, global.options)
+})
 
 export default function (ctx, inject) {
   inject('toast', Vue.toasted)
