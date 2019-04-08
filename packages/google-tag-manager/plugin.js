33ae3702ea7f359392fcceaa1d7038601e101ede
syffs <th3_on3@msn.com>
2019-04-08 14:20:10 +0200

    refactor(gtm): moved all logic to `gtm.js` which is now injected

--- packages/google-tag-manager/plugin.js
+++ packages/google-tag-manager/plugin.js
@@ -1,31 +1,11 @@
-// Include Google Tag Manager Script
-function setup() {
-  window['<%= options.layer %>'] = window['<%= options.layer %>'] || [];
-  window['<%= options.layer %>'].push({
-    event: 'gtm.js', 'gtm.start': new Date().getTime()
-  });
+import Gtm from './gtm'
+
+export default function(ctx, inject) {
+  const options = <%= JSON.stringify(options) %>
+
+  // Create a new Auth instance
+  const $gtm = new Gtm(ctx, options)
+  inject('gtm', $gtm)
+
+  $gtm.init()
 }
-
-const shouldCheckDNT = <%= options.respectDoNotTrack %>
-// Detect Do Not Track settings
-const hasDNT = shouldCheckDNT && (window.doNotTrack === '1'
-  || navigator.doNotTrack === 'yes'
-  || navigator.doNotTrack === '1'
-  || navigator.msDoNotTrack === '1'
-  || (window.external && window.external.msTrackingProtectionEnabled && window.external.msTrackingProtectionEnabled())
-)
-
-if (!hasDNT) setup()
-
-<% if (options.pageTracking) { %>
-// Every time the route changes (fired on initialization too)
-export default ({ app: { router } }) => {
-    if (!hasDNT) {
-      router.afterEach((to, from) => {
-        setTimeout(() => {
-          window['<%= options.layer %>'].push(to.gtm || { event: 'nuxtRoute', pageType: 'PageView', pageUrl: to.fullPath, routeName: to.name })
-        }, 0)
-      })
-    }
-  }
-<% } %>
