0290da4bc937965dc0a3014b34fcf2567136d3d0
naumstory <mail.long33@gmail.com>
2019-09-29 00:12:46 +0500

    Fix async and add preload

--- packages/yandex-metrika/index.js
+++ packages/yandex-metrika/index.js
@@ -1,19 +1,36 @@
-const path = require('path')
+const path = require('path');
 
-module.exports = function yandexMetrika (options) {
+module.exports = function yandexMetrika(options) {
   // Don't include on dev mode
   if (this.options.dev && process.env.NODE_ENV !== 'production') {
-    return
+    return;
   }
+
+  // Script preload
+  this.options.head.link.push({
+    href:
+      (options.useCDN
+        ? 'https://cdn.jsdelivr.net/npm/yandex-metrica-watch'
+        : 'https://mc.yandex.ru/metrika') + '/watch.js',
+    rel: 'preload',
+    as: 'script'
+  });
 
   // Add yandex metrika script to head
   this.options.head.script.push({
-    src: (options.useCDN ? 'https://cdn.jsdelivr.net/npm/yandex-metrica-watch' : 'https://mc.yandex.ru/metrika') + '/watch.js', // add https://cdn.jsdelivr.net/npm/yandex-metrica-watch/watch.js
-    async: ''
-  })
+    src:
+      (options.useCDN
+        ? 'https://cdn.jsdelivr.net/npm/yandex-metrica-watch'
+        : 'https://mc.yandex.ru/metrika') + '/watch.js', // add https://cdn.jsdelivr.net/npm/yandex-metrica-watch/watch.js
+    async: 'true'
+  });
 
   // Register plugin
-  this.addPlugin({src: path.resolve(__dirname, 'plugin.js'), ssr: false, options})
-}
+  this.addPlugin({
+    src: path.resolve(__dirname, 'plugin.js'),
+    ssr: false,
+    options
+  });
+};
 
-module.exports.meta = require('./package.json')
+module.exports.meta = require('./package.json');
