936d0a25f7f03f479976af22978c53de66125512
Takuma HANATANI <mail@potato4d.me>
2019-11-01 18:39:37 +0900

    feat(toast): add `$toast` type definition to vuex context and application options (#326)

--- packages/toast/index.d.ts
+++ packages/toast/index.d.ts
@@ -1,6 +1,25 @@
 import Vue from 'vue'
 import { Toasted } from 'vue-toasted'
 
+declare module '@nuxt/vue-app' {
+  interface Context {
+    $toast: Toasted
+  }
+  interface NuxtAppOptions {
+    $toast: Toasted
+  }
+}
+
+// Nuxt 2.9+
+declare module '@nuxt/types' {
+  interface Context {
+    $toast: Toasted
+  }
+  interface NuxtAppOptions {
+    $toast: Toasted
+  }
+}
+
 declare module 'vue/types/vue' {
   interface Vue {
     $toast: Toasted
