0190183f463e41b5dcc23c05c220f56c877de87a
Sebastian Krüger <2pi_r2@gmx.de>
2020-02-05 22:54:53 +0100

    Fixing #216

--- packages/markdownit/index.js
+++ packages/markdownit/index.js
@@ -60,7 +60,7 @@ module.exports = function nuxtMarkdownit (options) {
     this.addPlugin({
       src: path.resolve(__dirname, 'plugin.js'),
       fileName: 'markdown-it.js',
-      options: _options
+      options: Object.asign({}, _options)
     })
   }
 }
