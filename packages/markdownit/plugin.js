776ecd1e9731d4c9a43554991a1ec48aff22fecc
Juho Rutila <juho.rutila@gmail.com>
2020-02-09 13:18:28 +0200

    fix(markdownit): `preset` configuration value was ignored (#340)

--- packages/markdownit/plugin.js
+++ packages/markdownit/plugin.js
@@ -4,12 +4,14 @@ const handlePlugin = (plugin) => plugin.default || plugin
 
 export default ({ app }, inject) => {
 <%
+const preset = options.preset || 'default'
+delete options.preset
 const plugins = options.use || []
 delete options.use
 options = serialize(options)
 options = options === '{}' ? undefined : options
 %>
-  const md = new MarkdownIt(<%= options %>)
+  const md = new MarkdownIt('<%= preset %>', <%= options %>)
 <%
   for (config of plugins) {
     const hasOpts = Array.isArray(config);
