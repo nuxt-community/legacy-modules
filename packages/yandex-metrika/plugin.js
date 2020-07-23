3e9711c61cf1be2a4c6c75c35c3838c721764626
Roman <25590253+x-range@users.noreply.github.com>
2020-07-23 14:40:19 +0700

    feat(yandex-metrika): update loading script (#371)

--- packages/yandex-metrika/plugin.js
+++ packages/yandex-metrika/plugin.js
@@ -7,12 +7,16 @@ export default ({ app: { router } }) => {
   })
 
   function create() {
-    ym(<%= options.id %>, "init", <%= JSON.stringify(options) %>);
+
+    if (!ready) {
+      // Don't record a duplicate hit for the initial navigation.
+      (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+        m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+      (window, document, "script", '<%= options.metrikaUrl %>', "ym")
+
+      ym(<%= options.id %>, "init", <%= JSON.stringify(options) %>)
+    }
     router.afterEach((to, from) => {
-      if (!ready) {
-        // Don't record a duplicate hit for the initial navigation.
-        return
-      }
       ym(<%= options.id %>, 'hit', to.fullPath, {
         referer: from.fullPath
         // TODO: pass title: <new page title>
@@ -22,14 +26,7 @@ export default ({ app: { router } }) => {
   }
 
   if (window.ym === undefined) {
-    // Yandex.Metrika has not loaded yet, create ym method.
-    (function (m, i, k, a) {
-      m[i] = m[i] || function () {
-        (m[i].a = m[i].a || []).push(arguments)
-      }
-      m[i].l = 1 * new Date()
-    })
-    (window, "ym")
+    create()
   }
-  create()
+
 }
